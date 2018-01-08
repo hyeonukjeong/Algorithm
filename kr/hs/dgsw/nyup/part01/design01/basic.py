ceeaf078cb120b60346239694189c47adf54484f
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-08 12:07:02 +0900

    20180108

---
+++ kr/hs/dgsw/nyup/part01/design01/basic.py
@@ -0,0 +1,46 @@
+'''
+1. 곱하기와 나누기
+'''
+
+print (3 * 2)
+print (3 / 2)
+print (3 ** 4)
+
+'''
+2. 할당문
+'''
+
+a = 1
+b = 2
+print (a + b)
+
+'''
+3. 문자
+'''
+
+c = "You need "
+d = 'Python.'
+print (c + d)
+
+'''
+4. 조건문
+'''
+
+e = 5
+if e > 3:
+    print ("e는 3보다 크다.")
+
+'''
+5. 반복문
+'''
+
+for f in [1, 3, 7]:
+    print (f)
+
+'''
+6. 함수
+'''
+
+def add (g, h):
+    return (g + h)
+print (add (3, 5))
