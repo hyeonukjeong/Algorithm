52a24431014b192bcda071a23e36faefd2f1a3bc
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-09 15:11:36 +0900

    20180109

--- kr/hs/dgsw/nyup/part01/design08/knightTable.py
+++ kr/hs/dgsw/nyup/part01/design08/knightTable.py
@@ -5,4 +5,12 @@ b_knight = bin(knight)
 # 생존 기사의 자리
 k_position = int(b_knight[3:] + b_knight[2], base = 2)
 # 출력
-print(k_position)
+print(knight, "명의 기사가 있을때 생존 기사의 자리는", k_position, "번째 자리입니다.")
+# 로그를 사용하기 위해 참조
+from math import log
+# 기사의 수
+m_knight = 12
+# 생존 기자의 자리
+m_k_position = (m_knight - 2 ** int(log(m_knight, 2))) * 2 + 1
+# 출력
+print(m_knight, "명의 기사가 있을때 생존 기사의 자리는", m_k_position, "번째 자리입니다.")
