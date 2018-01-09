2629a5e3302b91744cf53b3d866308b104236ea1
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-09 10:46:05 +0900

    20180109

---
+++ kr/hs/dgsw/nyup/classroom/basecamp.py
@@ -0,0 +1,10 @@
+# 자전거의 시속
+bike = 20
+# 도보의 시속
+walk = 4
+# 걸린 시간의 차
+difference = 1
+# 거리
+distance = bike * walk * difference / (bike - walk)
+# 출력
+print("베이스 캠프 1과 베이스 캠프 2는 ", distance, "km 만큼 떨어져 있습니다.")
---
+++ kr/hs/dgsw/nyup/part01/design02/calc.py
@@ -0,0 +1,20 @@
+''' plus '''
+print (5 + 3)
+
+''' minus '''
+print (5 - 3)
+
+''' asterisk '''
+print (5 * 3)
+
+''' double asterisk '''
+print (5 ** 3)
+
+''' slash '''
+print (5 / 3)
+
+''' double slash '''
+print (5 // 3)
+
+''' percent '''
+print (5 % 3)
