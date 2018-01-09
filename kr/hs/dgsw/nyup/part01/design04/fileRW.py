9ee955eeb13f2a62352203e9e5bb2deb2e4ea846
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-09 13:31:43 +0900

    20180109

--- kr/hs/dgsw/nyup/part01/design04/fileRW.py
+++ kr/hs/dgsw/nyup/part01/design04/fileRW.py
@@ -1,12 +1,16 @@
 # 입력 파일 경로
 fin = open('./input.txt')
+fout = open('./output.txt','w')
 # 1줄씩 읽기
-a = int(fin.readline())
-b = int(fin.readline())
-c = int(fin.readline())
+a = fin.readline()
+b = fin.readline()
+c = fin.readline()
+w = '{0:s} {1:s} {2:s}'.format(a, b, c)
 # 출력
 print("a가 가져간 금화의 수 :", a)
 print("b가 가져간 금화의 수 :", b)
 print("c가 가져간 금화의 수 :", c)
+fout.write(w)
 # 파일 닫기
 fin.close()
+fout.close()
