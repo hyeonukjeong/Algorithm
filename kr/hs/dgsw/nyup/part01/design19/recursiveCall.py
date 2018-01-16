28aabd68bc725dd9cf951714b40142cd121ed466
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-16 11:08:39 +0900

    20180116

---
+++ kr/hs/dgsw/nyup/part01/design19/recursiveCall.py
@@ -0,0 +1,29 @@
+def f1(n):
+    global s
+    if n == 0:
+        return 0
+    s += n
+    f1(n - 1)
+    return s
+
+def f2(n):
+    if n == 0:
+        return 0
+    s = n + f2(n - 1)
+    return s
+
+def f3(n):
+    if n == 0:
+        return 0
+    return n + f3(n - 1)
+
+def f4(n):
+    if n == -1:
+        return 0
+    return n +f4(n - 1)
+
+s = 0
+print(f1(10))
+print(f2(10))
+print(f3(10))
+print(f4(10))
