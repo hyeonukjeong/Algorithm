1d45920a83cecd47ed4db58fcb593173b93e119b
JEONGHYEONUK <hujeong1004@dgsw.hs.kr>
2018-01-17 14:31:31 +0900

    20180117

--- kr/hs/dgsw/nyup/part01/design22/star.py
+++ kr/hs/dgsw/nyup/part01/design22/star.py
@@ -17,4 +17,14 @@ for i in range(0, 721, 144):
     y = r * m.cos(a)
     t.goto(x, y)
 
+t.pencolor('white')
+t.width(3)
+t.up(); t.goto(0, 360); t.down()
+
+for i in range(0, 721, 144):
+    a = p * i / 180.0
+    x = r * m.sin(a)
+    y = r * m.cos(a)
+    t.goto(x, y)
+
 t.done()
